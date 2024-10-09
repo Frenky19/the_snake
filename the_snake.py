457169dd84589c6e1ec60dc2d080f6915295e5dd
Andrey Golovushkin <102225757+Frenky19@users.noreply.github.com>
2024-10-09 20:39:40 +0300

    Working on mistakes

--- the_snake.py
+++ the_snake.py
@@ -50,80 +50,118 @@ clock = pg.time.Clock()
 class GameObject:
     """Класс родитель с общими аргументами и методами."""
 
-    def __init__(self, position=DEFAULT_POSITION, body_color=BOARD_BACKGROUND_COLOR) -> None:
-        """"""
+    def __init__(
+        self,
+        position=DEFAULT_POSITION,
+        body_color=BOARD_BACKGROUND_COLOR
+    ) -> None:
+        """Конструктор главного класса."""
         self.position = position
         self.body_color = body_color
 
+    def draw_base(
+        self,
+        screen,
+        position=None,
+        body_color=None,
+        border_color=BORDER_COLOR
+    ):
+        """Метод для отрисовки ячейки на экране."""
+        if position is None:
+            position = self.position
+        if body_color is None:
+            body_color = self.body_color
+        rect = pg.Rect(position[0], position[1], GRID_SIZE, GRID_SIZE)
+        pg.draw.rect(screen, body_color, rect)
+        pg.draw.rect(screen, border_color, rect, 1)
+
     def draw(self):
-        """Заглушка, будет переопределен в дочерних классах"""
+        """Заглушка, будет переопределен в дочерних классах."""
         raise NotImplementedError
 
 
-class Apple(GameObject):
+class InteactiveObject(GameObject):
+    """
+    Базовый класс для интерактивных объектов: Яблоко и Ёжик.
+
+    randomize_position -- выбирает случайную ячейку для спавна
+    draw -- переопределенный метод, отрисовывающий объект
+    """
+
+    def __init__(
+        self,
+        occupied_positions_snake=DEFAULT_POSITION,
+        body_color=BOARD_BACKGROUND_COLOR
+    ):
+        """Конструктор классов для взаимодействия."""
+        super().__init__(body_color=body_color)
+        self.randomize_position(occupied_positions_snake)
+
+    def randomize_position(self, occupied_positions_snake=DEFAULT_POSITION):
+        """Задает случайную ячейку для появления объекта."""
+        if not occupied_positions_snake:
+            occupied_positions_snake = []
+        while True:
+            self.position = (randrange(0, GRID_WIDTH) * GRID_SIZE), (
+                randrange(0, GRID_HEIGHT) * GRID_SIZE)
+            if self.position not in occupied_positions_snake:
+                break
+
+    def draw(self, screen, position, body_color):
+        """Отрисовка объекта."""
+        self.draw_base(screen, position, body_color)
+
+
+class Apple(InteactiveObject):
     """
     Дочерний класс Яблоко.
 
     Если голова Змейки попадет на ячейку с
     Яблоком,то ее длина вырастет на 1.
-    randomize_position -- выбирает случайную ячейку для спавна
-    draw -- переопределенный метод, отрисовывающий Яблоко
     """
 
-    def __init__(self, occupied_positions=DEFAULT_POSITION, body_color=APPLE_COLOR):
-        """"""
-        super().__init__(body_color=body_color)
-        self.randomize_position(occupied_positions)
-
-    def randomize_position(self, occupied_positions=DEFAULT_POSITION):
-        """Выбирает случайную ячейку для появления Яблока."""
-        if occupied_positions is None:
-            occupied_positions = []
-        while True:
-            position = (randrange(0, GRID_WIDTH) * GRID_SIZE), (
-                randrange(0, GRID_HEIGHT) * GRID_SIZE)
-            if position not in occupied_positions:
-                self.position = position
-                break
-
-    def draw(self):
-        """Метод отрисовывающий Яблоко на экране."""
-        rect = pg.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-        pg.draw.rect(screen, self.body_color, rect)
-        pg.draw.rect(screen, BORDER_COLOR, rect, 1)
+    def __init__(self, occupied_positions_snake=DEFAULT_POSITION):
+        """Конструтор класса Яблоко"""
+        super().__init__(occupied_positions_snake, APPLE_COLOR)
 
 
-class Hedgehog(GameObject):
+class Hedgehog(InteactiveObject):
     """
     Дочерний класс Ёжик.
 
     Если голова Змейки попадет на ячейку с
     Ёжиком,то ее длина уменьшится на 1.
-    randomize_position -- выбирает случайную ячейку для спавна
-    draw -- переопределенный метод, отрисовывающий Ёжика
+    randomize_position -- выбирает случайную ячейку
     """
 
-    def __init__(self, occupied_positions=DEFAULT_POSITION, body_color=HEDGEHOG_COLOR):
-        """"""
-        super().__init__(body_color=body_color)
-        self.randomize_position(occupied_positions)
+    def __init__(
+        self,
+        occupied_positions_snake=DEFAULT_POSITION,
+        occupied_positions_apple=None
+    ):
+        """Конструктор класса Ёжик"""
+        super().__init__(occupied_positions_snake, HEDGEHOG_COLOR)
+        self.randomize_position(
+            occupied_positions_snake,
+            occupied_positions_apple
+        )
 
-    def randomize_position(self, occupied_positions=DEFAULT_POSITION):
-        """Выбирает случайную ячейку для появления Ёжика."""
-        if occupied_positions is None:
-            occupied_positions = []
+    def randomize_position(
+        self,
+        occupied_positions_snake=DEFAULT_POSITION,
+        occupied_positions_apple=None
+    ):
+        """Метод, выбирающий свободную от Змейки и Яблока позицию ячейку"""
+        if not occupied_positions_snake:
+            occupied_positions_snake = []
         while True:
-            position = (randrange(0, GRID_WIDTH) * GRID_SIZE), (
-                randrange(0, GRID_HEIGHT) * GRID_SIZE)
-            if position not in occupied_positions:
-                self.position = position
+            self.position = (
+                randrange(0, GRID_WIDTH) * GRID_SIZE,
+                randrange(0, GRID_HEIGHT) * GRID_SIZE
+            )
+            if (self.position not in occupied_positions_snake
+               and self.position != occupied_positions_apple):
                 break
-
-    def draw(self):
-        """Метод отрисовывающий Ёжика на экране."""
-        rect = pg.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-        pg.draw.rect(screen, self.body_color, rect)
-        pg.draw.rect(screen, BORDER_COLOR, rect, 1)
 
 
 class Snake(GameObject):
@@ -138,7 +176,7 @@ class Snake(GameObject):
     """
 
     def __init__(self, body_color=SNAKE_COLOR):
-        """"""
+        """Конструктор класса Змейка."""
         super().__init__(body_color=body_color)
         self.reset()
 
@@ -164,21 +202,25 @@ class Snake(GameObject):
         """Позиция первого элемента (головы) Змейки."""
         return self.positions[0]
 
+    def get_tail_position(self):
+        """Позиция последнего элемента (хвоста) Змейки"""
+        return self.positions[-1]
+
     def draw(self):
         """Метод отрисовывающий Змейку на экране."""
-        for position in self.positions:
-            head_rect = pg.Rect(self.get_head_position(), (GRID_SIZE, GRID_SIZE))
-            pg.draw.rect(screen, self.body_color, head_rect)
-            pg.draw.rect(screen, BORDER_COLOR, head_rect, 1)
-            rect = (pg.Rect(position, (GRID_SIZE, GRID_SIZE)))
-            pg.draw.rect(screen, self.body_color, rect)
-            pg.draw.rect(screen, BORDER_COLOR, rect, 1)
+        head_rect = pg.Rect(self.get_head_position(), (GRID_SIZE, GRID_SIZE))
+        pg.draw.rect(screen, self.body_color, head_rect)
+        pg.draw.rect(screen, BORDER_COLOR, head_rect, 1)
+        tail_rect = pg.Rect(self.get_tail_position(), (GRID_SIZE, GRID_SIZE))
+        pg.draw.rect(screen, self.body_color, tail_rect)
+        pg.draw.rect(screen, BORDER_COLOR, tail_rect, 1)
 
     def reset(self):
         """
         Метод, отвечающий за начало новой
         партии при окончании предыдущей.
         """
+        screen.fill(BOARD_BACKGROUND_COLOR)
         self.length = 1
         self.positions = [self.position]
         self.direction = RIGHT
@@ -210,32 +252,58 @@ def main():
     # Экземпляры классов
     snake = Snake()
     apple = Apple(snake.positions)
-    hedgehog = Hedgehog(snake.positions)
-    #  screen.fill(BOARD_BACKGROUND_COLOR)
+    hedgehog = Hedgehog(snake.positions, apple.position)
+    # Создаем фон
+    screen.fill(BOARD_BACKGROUND_COLOR)
+    # Бесконечный цикл
     while True:
         # Увеличение скорости в зависимости от длины змеи
         speed = SPEED + len(snake.positions) // 5
         clock.tick(speed)
-        screen.fill(BOARD_BACKGROUND_COLOR)
         handle_keys(snake)
+        # Запоминаем последнее значение "хвоста"
+        last_position = snake.positions[-1]
         snake.move()
+        # Затираем ячейку в цвет фона после движения Змейки
+        if last_position:
+            pg.draw.rect(
+                screen,
+                BOARD_BACKGROUND_COLOR,
+                pg.Rect(last_position[0], last_position[1],
+                        GRID_SIZE, GRID_SIZE)
+            )
+        # Проверка на столкновение с Яблоком
         if snake.get_head_position() == apple.position:
             snake.length += 1
-            apple.randomize_position()
+            apple.randomize_position(snake.positions)
+        # Проверка на стокновение с Ёжиком
         if snake.get_head_position() == hedgehog.position:
             snake.length -= 1
-            snake.positions.pop()
+            if snake.positions:
+                last_position = snake.positions.pop()
+                # Затирка ячейки, где находился Ёжик
+                pg.draw.rect(
+                    screen,
+                    BOARD_BACKGROUND_COLOR,
+                    pg.Rect(last_position[0], last_position[1],
+                            GRID_SIZE, GRID_SIZE)
+                )
             if len(snake.positions) == 0:
                 snake.reset()
-                apple.randomize_position()
-            hedgehog.randomize_position()
+                apple.randomize_position(snake.positions)
+            hedgehog.randomize_position(snake.positions, apple.position)
+        # Проверка на стокновение самой с собой
         if snake.get_head_position() in snake.positions[1:]:
             snake.reset()
-            apple.randomize_position()
-            hedgehog.randomize_position()
+            apple.randomize_position(snake.positions)
+            hedgehog.randomize_position(snake.positions, apple.position)
         snake.draw()
-        apple.draw()
-        hedgehog.draw()
+        apple.draw(screen, position=apple.position, body_color=APPLE_COLOR)
+        hedgehog.draw(
+            screen,
+            position=hedgehog.position,
+            body_color=HEDGEHOG_COLOR
+        )
         pg.display.update()
 
 
